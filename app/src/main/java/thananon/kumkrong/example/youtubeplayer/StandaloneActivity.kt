85a6b401b2596483e8f195b13c8af41ea2c68cc2
Thananon Kumkrong <thananon.oat@gmail.com>
2019-10-25 20:32:10 +0700

    Create StandaloneActivity, showing various ways of setting onClick listener

---
+++ app/src/main/java/thananon/kumkrong/example/youtubeplayer/StandaloneActivity.kt
@@ -0,0 +1,45 @@
+package thananon.kumkrong.example.youtubeplayer
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.youtube.player.YouTubeStandalonePlayer
+import kotlinx.android.synthetic.main.activity_standalone.*
+
+class StandaloneActivity: AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_standalone)
+
+        btnPlayVideo.setOnClickListener(this)
+        btnPlaylist.setOnClickListener(this)
+
+//        btnPlayVideo.setOnClickListener(object: View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//        })
+
+//        btnPlayVideo.setOnClickListener(View.OnClickListener { v ->
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        })
+
+//        val listener = View.OnClickListener { v ->
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        }
+//        btnPlayVideo.setOnClickListener(listener)
+//        btnPlaylist.setOnClickListener(listener)
+    }
+
+    override fun onClick(view: View) {
+        val intent = when (view.id) {
+            R.id.btnPlayVideo -> YouTubeStandalonePlayer.createVideoIntent(
+                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_VIDEO_ID)
+            R.id.btnPlaylist -> YouTubeStandalonePlayer.createPlaylistIntent(
+                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_PLAYLIST)
+            else -> throw IllegalArgumentException("Undefined button clicked")
+        }
+        startActivity(intent)
+    }
+}
