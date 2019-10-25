bb6a32dcfd2629a5a30559f8a549077d6d9d102f
Thananon Kumkrong <thananon.oat@gmail.com>
2019-10-25 09:45:56 +0700

    Add activity. Right package -> New -> Activity -> Empty Activity. Use YouTubeBaseActivity instead of AppCompatActivity. Use YouTubePlayer.OnInitializedListener. Use Alt + Insert to implement help implement interface.

---
+++ app/src/main/java/thananon/kumkrong/example/youtubeplayer/YoutubeActivity.kt
@@ -0,0 +1,29 @@
+package thananon.kumkrong.example.youtubeplayer
+
+import android.os.Bundle
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+
+class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
