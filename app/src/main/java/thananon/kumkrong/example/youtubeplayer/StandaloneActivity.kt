acb04757faf13681901b82708431620296f6560a
Thananon Kumkrong <thananon.oat@gmail.com>
2019-10-26 09:30:58 +0700

    Challenge Video 126: Use docs on YouTube Standalone Player to make video in StandaloneActivity play automatically.

--- app/src/main/java/thananon/kumkrong/example/youtubeplayer/StandaloneActivity.kt
+++ app/src/main/java/thananon/kumkrong/example/youtubeplayer/StandaloneActivity.kt
@@ -35,9 +35,9 @@ class StandaloneActivity: AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View) {
         val intent = when (view.id) {
             R.id.btnPlayVideo -> YouTubeStandalonePlayer.createVideoIntent(
-                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_VIDEO_ID)
+                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_VIDEO_ID, 0, true, false)
             R.id.btnPlaylist -> YouTubeStandalonePlayer.createPlaylistIntent(
-                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_PLAYLIST)
+                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_PLAYLIST, 0, 0, true, false)
             else -> throw IllegalArgumentException("Undefined button clicked")
         }
         startActivity(intent)
